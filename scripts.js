279fa22b486e5d10e437ffc03314814001bcc70c
wraithy <wraith.andrew@gmail.com>
2015-03-13 22:52:50 +0200

    changed logic for everything to be on one svg

--- scripts.js
+++ scripts.js
@@ -1,48 +1,69 @@
-// set up static stuff which doesn't need data
-var numSchools = 100,
-	margin = {top: 20, right: 30, bottom: 30, left: 100},
-	width = 1000 - margin.left - margin.right,
-	height = 300 - margin.top - margin.bottom,
+// changeable vars
+var numSchools = 20,
+	width = 1000,
+	height = 800,
 	barWidth = 3;
 
-var data = [],
-	xScale = d3.scale.linear(),
-	yScale = d3.scale.linear();
+// set up static stuff which doesn't need data
+var	axisWidth = 100,
+	axisHeight = height - 100,
+	graphHeight = axisHeight/2,
+	graphWidth = width - axisWidth;
 
-var chart = d3.select("#vis")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+var vis = d3.select("#vis")
+	.attr("width", width)
+	.attr("height", height);
+
+var xScale = d3.scale.linear(),
+	yScale = d3.scale.linear();
 
 var xAxis = d3.svg.axis()
 	.scale(xScale)
-	.orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
+	.orient("bottom"),
+	
+	yAxis = d3.svg.axis()
+	.scale(yScale)
+	.orient("left")
 	.ticks(3, "$,");
 
 // get the data and set up everything else
 d3.json("/data.json", function(d){
-	// grab the top schools
-	for (i=0; i<numSchools; i++) {
-		data.push(d[i]);
-	}
-	
-	data.sort(compare);
+	data = getTop(d, numSchools);
+	data.sort(geoSort);
+	makeScales(data);
+	makeMap();
+	makeGraph();
+	makeAxes();
+});
 
+
+function getTop(d, n) {
+	result = [];
+	for (i=0; i<n; i++) {
+		result.push(d[i]);
+	}
+	return result;
+}
+
+function geoSort(a, b) {
+	if (a.Geolocation.lng < b.Geolocation.lng)
+		 return -1;
+	if (a.Geolocation.lng > b.Geolocation.lng)
+		return 1;
+	return 0;
+}
+
+function makeScales(d) {
 	yScale.domain([ 
-		d3.min(data, function(d){
-			return +d["Early Career Median Pay (0-5 YE)"];
-		}),
-		//0,
+		//d3.min(data, function(d){
+			//return +d["Early Career Median Pay (0-5 YE)"];
+		//}),
+		0,
 		d3.max(data, function(d){
 			return +d["Early Career Median Pay (0-5 YE)"];
 		})
 	]);
-	yScale.range([height, 0]);
+	yScale.range([graphHeight, 0]);
 
 	xScale.domain([
 		d3.min(data, function(d){
@@ -52,44 +73,43 @@ d3.json("/data.json", function(d){
 		d3.max(data, function(d){
 			return d.Geolocation.lng;
 		})
-	])
-	xScale.range([0, width])
-
-	// add the axes
-	chart.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-
-	chart.append("g")
-		.attr("class", "y axis")
-		.call(yAxis)
-
-	makeVis();
-});
-
-function compare(a, b) {
-	if (a.Geolocation.lng < b.Geolocation.lng)
-		 return -1;
-	if (a.Geolocation.lng > b.Geolocation.lng)
-		return 1;
-	return 0;
+	]);
+	xScale.range([0, graphWidth-10]);
 }
 
-function makeVis(){
-	chart.selectAll("g")
+function makeMap() {
+	vis.append("image")
+		.attr("xlink:href", "map.svg")
+		.attr("width", width)
+		.attr("height", graphHeight)
+}
+
+function makeGraph() {
+	vis.selectAll("g")
 		.data(data)
 		.enter().append("g")
 		.attr("transform", function(d, i) {
-			return "translate(" + xScale(d.Geolocation.lng) + ",0)";
+			return "translate(" + (axisWidth+xScale(d.Geolocation.lng)) + ",0)";
 		})
 		.append("rect")
 		.attr("y", function(d) {
-			return height - yScale(+d["Early Career Median Pay (0-5 YE)"]);
+			return axisHeight - yScale(+d["Early Career Median Pay (0-5 YE)"]);
 		})
 		.attr("height", function(d) {
 			return yScale(+d["Early Career Median Pay (0-5 YE)"]);
 		})
 		.attr("width", barWidth - 1);
+}
+
+function makeAxes() {
+	vis.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate("+axisWidth+","+axisHeight+")")
+		.call(xAxis);
+
+	vis.append("g")
+		.attr("class", "y axis")
+		.attr("transform", "translate("+(axisWidth-10)+","+graphHeight+")")
+		.call(yAxis);
 }
 
