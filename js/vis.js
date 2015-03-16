37e2ad849ed4babacfa2df7c3eef01c02dc3916c
wraithy <wraith.andrew@gmail.com>
2015-03-16 02:50:46 +0200

    finished visualisation

--- js/vis.js
+++ js/vis.js
@@ -1,13 +1,15 @@
 // changeable vars
 var numSchools = 50,
 	width = 1300,
-	height = 800;
-	barWidth = 5;
+	height = 650,
+	barWidth = 5,
+	earlyColor = "#69D2E7",
+	midColor = "#FA6900"
 
 // set up static stuff which doesn't need data
 var legendWidth = 300;
 	axisWidth = 150,
-	axisHeight = height - 100,
+	axisHeight = height - 10,
 	graphWidth = width - legendWidth - axisWidth - 10,
 	graphHeight = graphWidth * 0.513;
 
@@ -28,17 +30,18 @@ var xAxis = d3.svg.axis()
 	.orient("left")
 	.ticks(6, "$,");
 
-// get the data and set up everything else
-var data;
-d3.json("/data.json", function(d){
-	data = getTop(d, numSchools);
+var sourceData, data, barTip, dotTip;
+function plot() {
+	// get the data and set up everything else
+	data = getTop(sourceData, numSchools);
 	data.sort(geoSort);
 	makeScales();
+	makeTips();
 	makeMap();
 	makeGraph();
 	makeAxes();
 	makeLegend();
-});
+}
 
 function makeScales() {
 	yScale.domain([0,	d3.max(data, function(d) {
@@ -73,6 +76,22 @@ function makeGraph() {
 
 	// mid career
 	groups.append("rect")
+		.on("mouseover", function(e) {
+			e["RECTMID"] = 1;
+			delete e["RECTEAR"];
+			delete e["CIRCLE"];
+			e.source = this;
+			dotTip.show(e);
+			barTip.show(e);
+		})
+		.on("mouseout", function(e) {
+			e["RECTMID"] = 1;
+			delete e["RECTEAR"];
+			delete e["CIRCLE"];
+			e.source = this;
+			dotTip.hide(e);
+			barTip.hide(e);
+		})
 		.attr("transform", function(d){
 			return "translate(0,"+(yScale(+d["Mid-Career Median Pay (10+ YE)"])+graphHeight)+")";
 		})
@@ -81,10 +100,26 @@ function makeGraph() {
 		})
 		.attr("opacity", 0.7)
 		.attr("width", barWidth)
-		.attr("fill", "#FA6900");
+		.attr("fill", midColor);
 	
 	// early career
 	groups.append("rect")
+		.on("mouseover", function(e) {
+			e["RECTEAR"] = 1;
+			delete e["RECTMID"];
+			delete e["CIRCLE"];
+			e.source = this;
+			dotTip.show(e);
+			barTip.show(e);
+		})
+		.on("mouseout", function(e) {
+			e["RECTEAR"] = 1;
+			delete e["RECTMID"];
+			delete e["CIRCLE"];
+			e.source = this;
+			dotTip.hide(e);
+			barTip.hide(e);
+		})
 		.attr("transform", function(d){
 			return "translate(0,"+(yScale(+d["Early Career Median Pay (0-5 YE)"])+graphHeight)+")";
 		})
@@ -93,7 +128,7 @@ function makeGraph() {
 		})
 		.attr("opacity", 0.9)
 		.attr("width", barWidth)
-		.attr("fill", "#69D2E7");
+		.attr("fill", earlyColor);
 	
 	// separation for bars
 	groups.append("rect")
@@ -107,6 +142,20 @@ function makeGraph() {
 	
 	// dots on map
 	groups.append("circle")
+		.on("mouseover", function(e) {
+			e["CIRCLE"] = 1;
+			delete e["RECTMID"];
+			delete e["RECTEAR"];
+			dotTip.show(e);
+			barTip.show(e);
+		})
+		.on("mouseout", function(e) {
+			e["CIRCLE"] = 1;
+			delete e["RECTMID"];
+			delete e["RECTEAR"];
+			dotTip.hide(e);
+			barTip.hide(e);
+		})
 		.attr("cy", function(d) {
 			return mapScale(d.Geolocation.lat);
 		})
@@ -195,7 +244,7 @@ function makeLegend() {
 
 	legend.append("rect")
 		.attr("transform", "translate("+legendShift +","+(midGraph - 38)+")")
-		.attr("fill", "#FA6900")
+		.attr("fill", midColor)
 		.attr("width", 20)
 		.attr("height", 20)
 		.attr("stroke-width", 0.5)
@@ -209,7 +258,7 @@ function makeLegend() {
 	var midGraph = graphHeight+(axisHeight-graphHeight)/2;
 	legend.append("rect")
 		.attr("transform", "translate("+legendShift +","+(midGraph + 2)+")")
-		.attr("fill", "#69D2E7")
+		.attr("fill", earlyColor)
 		.attr("width", 20)
 		.attr("height", 20)
 		.attr("stroke-width", 0.5)
@@ -220,11 +269,58 @@ function makeLegend() {
 		.append("text").text("Early Career")
 }
 
+// welcome to last-minute programming, where nothing is real and everything is a hack!
+function makeTips() {
+	barTip = d3.tip()
+		.attr("class", "d3-tip")
+		.offset(function(d) {
+			if(d["CIRCLE"]) { // we are being triggered by a circle
+				var offset = graphHeight - mapScale(d.Geolocation.lat);
+				return [offset-10,0];
+			}
+			else if(d["RECTEAR"]) {
+				var early = +d.source.__data__["Early Career Median Pay (0-5 YE)"];
+				return [-yScale(early)-10, 0];
+			}
+			else if(d["RECTMID"]) {
+				var mid = +d.source.__data__["Mid-Career Median Pay (10+ YE)"];
+				return [-yScale(mid)-10, 0];
+			}
+			return [-10, 0];
+		})
+		.html(function(d) {
+			var mid = "$"+addCommas(d["Mid-Career Median Pay (10+ YE)"]);
+			var early = "$"+addCommas(d["Early Career Median Pay (0-5 YE)"]);
+			return "<span style='color:"+midColor+"'> Mid:\&nbsp\&nbsp</span> "+mid+"<br><span style='color:"+earlyColor+"'>Early: </span>"+early;
+		});
+
+	vis.call(barTip);
+	
+	dotTip = d3.tip()
+		.attr("class", "d3-tip")
+		.offset(function(d) {
+			if(d["RECTMID"]) {
+				var offset = mapScale(d.Geolocation.lat) - graphHeight - yScale(d["Mid-Career Median Pay (10+ YE)"]);
+				return [offset-30,0];
+			}
+			else if(d["RECTEAR"]) {
+				var offset = mapScale(d.Geolocation.lat) - graphHeight - yScale(d["Early Career Median Pay (0-5 YE)"]);
+				return [offset-30,0];
+			}
+			return [-30,0]
+		})
+		.html(function(d) {
+			return "<strong>"+d["School Name"]+"</strong><br><span style='color:#FF7BD1'>Rank: </span>"+d["Rank"]+"<br>"+d["School Type"];
+		});
+
+	vis.call(dotTip);
+}
+
 // wizardry.
 d3.selection.prototype.moveToFront = function() {
-  return this.each(function(){
-    this.parentNode.appendChild(this);
-  });
+	return this.each(function(){
+		this.parentNode.appendChild(this);
+	  });
 };
 
 var selection;
@@ -235,7 +331,7 @@ function makeInteract() {
 			d3.select(this).moveToFront();
 			selection.children().attr("class", "dim");
 			$(e.target).parent().children().attr("class", "highlight");
-			$(e.target).parent().children().attr("r", 10);
+			$(e.target).parent().children().attr("r", 12);
 		},
 		function(e) {
 			selection.children().attr("class", "");
@@ -264,4 +360,51 @@ function colorMap(x) {
 	else if (x <= 20) return "#ff7bd1";
 	else return "#ffdde0";
 }
+
+function addCommas(nStr)
+{
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+$('input[type="range"]').rangeslider({
+
+    // Feature detection the default is `true`.
+    // Set this to `false` if you want to use
+    // the polyfill also in Browsers which support
+    // the native <input type="range"> element.
+    polyfill: false,
+
+    // Default CSS classes
+    rangeClass: 'rangeslider',
+    fillClass: 'rangeslider__fill',
+    handleClass: 'rangeslider__handle',
+
+    // Callback function
+    onInit: function() {},
+
+    // Callback function
+	onSlide: function(position, value) {
+		$(".output").text("Number of schools ("+value+")");
+	},
+
+    // Callback function
+	onSlideEnd: function(position, value) {
+		numSchools = value;
+		$("#vis").empty();
+		plot();
+	}
+});
+
+d3.json("/data.json", function(d){
+	sourceData = d;
+	plot();
+});
 
